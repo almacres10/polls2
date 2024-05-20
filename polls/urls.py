4c2dabfd2546e32bd13fe006fc5be5a190d049e5
Marco TB <alvinmarco8@gmail.com>
2024-05-20 22:15:48 +0700

    update

--- polls/urls.py
+++ polls/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
-]
+    path("login/", views.UserLogin, name="login"),
+    path("signup/", views.UserSignUp, name="signup"),
+    path("logout/", views.UserLogout, name="logout"),
+]   
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,44 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse
 from django.db.models import F
 from . models import Question, Choice
 from django.utils import timezone
+from .forms import SignUpForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
+def UserSignUp(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('polls:index')
+    else:
+        form = SignUpForm()
+    return render(request, 'polls/signup.html', {'form':form})
+
+def UserLogin(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
+        else:
+            form = LoginForm()
+        return render(request, 'login.html', {'form':form})
+
+# logout page
+def UserLogout(request):
+    logout(request)
+    return redirect('UserLogin')
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
